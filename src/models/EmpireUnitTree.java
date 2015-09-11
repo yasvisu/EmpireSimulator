8121a4f15ea0d92983bc97e92970812ddd649992
yasvisu <yasvisu@gmail.com>
2015-09-11 13:11:45 +0300

    EmpireUnitTree implements contracts.UnitTree. #2

---
+++ src/models/EmpireUnitTree.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import contracts.Unit;
+import contracts.UnitTree;
+
+public class EmpireUnitTree implements UnitTree {
+
+	private Unit root;
+
+	public EmpireUnitTree() {
+
+	}
+
+	@Override
+	public Unit getRootUnit() {
+		return this.root;
+	}
+
+	public void setRootUnit(Unit root) {
+		this.root = root;
+	}
+
+	public Unit[] getAllUnits() {
+		LinkedList<Unit> result = new LinkedList<Unit>();
+
+		this.addDescendantsRecursively(this.root, result);
+
+		return result.toArray(new Unit[result.size()]);
+	}
+
+	private void addDescendantsRecursively(Unit current, List<Unit> list) {
+		if (current == null) {
+			return;
+		}
+
+		list.add(current);
+		for (Unit u : current.getOutputUnits()) {
+			addDescendantsRecursively(u, list);
+		}
+	}
+}
