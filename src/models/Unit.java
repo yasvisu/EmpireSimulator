faeda4ac16d227a167d2f15e149fcf881b63e533
yasvisu <yasvisu@gmail.com>
2015-09-11 11:58:59 +0300

    Added abstract Unit class. #3

--- src/models/Unit.java
+++ src/models/Unit.java
@@ -1,9 +1,39 @@
 package models;
 
-public class Unit {
-/* precision
- * exponent
- * flavor text
- * 
- */
+public abstract class Unit {
+
+	private double precision;
+	private int exponent;
+	private String flavorText;
+
+	protected Unit(double precision, int exponent, String flavorText) {
+		this.setPrecision(precision);
+		this.setExponent(exponent);
+		this.setFlavorText(flavorText);
+	}
+
+	public double getPrecision() {
+		return precision;
+	}
+
+	public void setPrecision(double precision) {
+		this.precision = precision;
+	}
+
+	public int getExponent() {
+		return exponent;
+	}
+
+	public void setExponent(int exponent) {
+		this.exponent = exponent;
+	}
+
+	public String getFlavorText() {
+		return flavorText;
+	}
+
+	private void setFlavorText(String flavorText) {
+		this.flavorText = flavorText;
+	}
+
 }
