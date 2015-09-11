a6647f9c3de3943b5a186d5418847aa118714be8
yasvisu <yasvisu@gmail.com>
2015-09-11 12:58:06 +0300

    EmpireEngine implements contracts.Engine. #2

---
+++ src/engine/EmpireEngine.java
@@ -0,0 +1,52 @@
+package engine;
+
+import contracts.Engine;
+import contracts.Unit;
+import contracts.UnitTree;
+import contracts.UpgradeTypes;
+
+public class EmpireEngine implements Engine {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public UnitTree getUnits() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean peekUpgrade(Unit unit, UpgradeTypes upgradeType) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean upgrade(Unit unit, UpgradeTypes upgradeType) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean peekBuyUnits(Unit unit, int count) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean buyUnits(Unit unit, int count) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void leapSeconds(long seconds) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
