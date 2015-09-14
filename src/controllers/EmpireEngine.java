f8b339ac6d97b44502c3e07051f2ad7e5684362b
yasvisu <yasvisu@gmail.com>
2015-09-14 19:54:48 +0300

    Added comments on the implementation of various EmpireEngine methods. #7

--- src/controllers/EmpireEngine.java
+++ src/controllers/EmpireEngine.java
@@ -8,70 +8,141 @@ import models.EmpireUnitTree;
 
 public class EmpireEngine implements Engine {
 
-	private UnitTree bacon;
-	private UnitTree freedom;
-	private UnitTree democracy;
-	private long money;
+    private UnitTree bacon;
+    private UnitTree freedom;
+    private UnitTree democracy;
+    private long money;
+    private long elapsedSeconds;
 
-	public EmpireEngine() {
-		this.bacon = new EmpireUnitTree();
-		this.freedom = new EmpireUnitTree();
-		this.democracy = new EmpireUnitTree();
-		this.money = 0;
+    public EmpireEngine() {
+	this.bacon = new EmpireUnitTree();
+	this.freedom = new EmpireUnitTree();
+	this.democracy = new EmpireUnitTree();
+	this.money = 0;
+	this.elapsedSeconds = 0;
+    }
+
+    @Override
+    public void update() {
+	// TODO Auto-generated method stub
+
+	long current = System.currentTimeMillis();
+	long deltaT = current - this.elapsedSeconds * 1000;
+	long deltaTSeconds = deltaT / 1000;
+	if (deltaTSeconds > 0) {
+	    /*
+	     * Parse through all trees (or other types of data); update all trees.
+	     * 
+	     * Bacon: parse tree from root; increase descendants with output
+	     * quantities. Freedom: parse tree from root; increase only Freedom
+	     * at the bottom. Increase democracy by: (senators' output) *
+	     * deltaTSeconds Money: do nothing (to be updated by dedicated
+	     * methods for selling / buying)
+	     */
+	}
+    }
+
+    @Override
+    public UnitTree getUnits(String resourceName) {
+	switch (resourceName) {
+	case "bacon":
+	    return this.bacon;
+	case "freedom":
+	    return this.freedom;
+	case "democracy":
+	    return this.democracy;
+	default:
+	    throw new IllegalArgumentException("No such resource.");
+	}
+    }
+
+    @Override
+    public boolean peekUpgrade(Unit unit, UpgradeTypes upgradeType) {
+	if (unit == null) {
+	    throw new IllegalArgumentException("Unit to check upgrades for should not be null.");
 	}
 
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
+	/*
+	 * Check whether unit can be upgraded. Use
+	 * Unit.getUpgradeLevel(upgradeType); Use
+	 * this.calculateUpgradeCost(upgradeType, level); if
+	 * Unit.getExponent()== cost exponent AND Unit.getPrecision() > cost
+	 * precision => true else false
+	 */
 
-	}
+	return false;
+    }
 
-	@Override
-	public UnitTree getUnits(String resourceName) {
-		switch (resourceName) {
-			case "bacon": return this.bacon;
-			case "freedom": return this.freedom;
-			case "democracy": return this.democracy;
-			default: throw new IllegalArgumentException("No such resource.");
-		}
-	}
+    @Override
+    public boolean upgrade(Unit unit, UpgradeTypes upgradeType) {
+	// TODO Auto-generated method stub
 
-	@Override
-	public boolean peekUpgrade(Unit unit, UpgradeTypes upgradeType) {
-		if (unit == null) {
-			throw new IllegalArgumentException("Unit to check upgrades for should not be null.");
-		}
+	/*
+	 * Use peekUpgrade(unit, upgradeType) to see if an upgrade is possible.
+	 * If true, set output with Unit.setOutput(number), or set spawncount
+	 * with Unit.setSpawnCount(number). Use
+	 * this.calculateUpgradeCost(upgradeType, level) to get the costs, then
+	 * subtract the costs from the appropriate units. Hint: Get the
+	 * "sacrifice unit" via previous unit.
+	 */
 
-		return false;
-	}
+	return false;
+    }
 
-	@Override
-	public boolean upgrade(Unit unit, UpgradeTypes upgradeType) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean peekBuyUnits(Unit unit, int count) {
+	// TODO Auto-generated method stub
 
-	@Override
-	public boolean peekBuyUnits(Unit unit, int count) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	/*
+	 * Calculate unit cost with count * Unit.getUnitCost() and count *
+	 * Unit.getResourceCost(). If there are resources and enough units,
+	 * return true. Hint: Get the "sacrifice unit" via Unit.getOutputUnits()
+	 * else false
+	 */
 
-	@Override
-	public boolean buyUnits(Unit unit, int count) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	return false;
+    }
 
-	@Override
-	public void leapSeconds(long seconds) {
-		// TODO Auto-generated method stub
+    @Override
+    public boolean buyUnits(Unit unit, int count) {
+	// TODO Auto-generated method stub
 
-	}
+	/*
+	 * Use peekBuyUnits(unit, count) to see if an purchase is possible. If
+	 * true, calculate # of new Units via count * Unit.getSpawnCount(). Then
+	 * modify the precision and exponent. Use
+	 * this.calculateUpgradeCost(upgradeType, level) to get the costs, then
+	 * subtract the costs from the appropriate units. Hint: Get the
+	 * "sacrifice unit" via previous unit.
+	 */
 
-	@Override
-	public long getScore() {
-	    // TODO Auto-generated method stub
-	    return 0;
-	}
+	return false;
+    }
+
+    @Override
+    public void leapSeconds(long seconds) {
+	// TODO Auto-generated method stub
+	/*
+	 * Leap the engine by a certain amount of seconds:
+	 * 
+	 * 1. DEcrease this.elapsedSeconds by the amount of seconds
+	 * 
+	 * 2. this.update();
+	 * 
+	 */
+    }
+
+    @Override
+    public long getScore() {
+	// TODO Auto-generated method stub
+	/*
+	 * Calculate the score of the player, based on:
+	 * 
+	 * - Resources - Units - Upgrades
+	 * 
+	 * (you decide how to weigh each)
+	 * 
+	 */
+	return 0;
+    }
 }
