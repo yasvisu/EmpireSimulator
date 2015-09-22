7f9f8cb5d418ede57d7e46e7e1fb4bf438e1fc26
Iliyan <iliqnvidenov92@gmail.com>
2015-09-22 14:54:51 +0300

    add some things

--- src/UI/BattlesSimulator.java
+++ src/UI/BattlesSimulator.java
@@ -1,5 +1,6 @@
 package ui;
 
+import contracts.Engine;
 import javafx.animation.Animation;
 import javafx.animation.TranslateTransitionBuilder;
 import javafx.beans.binding.Bindings;
@@ -14,9 +15,12 @@ import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import models.HugeInteger;
 import models.guiModels.Missile;
 import models.guiModels.Planet;
 import utils.Constants;
+import utils.EnhancedRandom;
+
 import java.util.*;
 
 
@@ -25,11 +29,11 @@ public class BattlesSimulator {
     private Animation missileAnimation;
     private Group missiles;
 
-    List<Planet> planets;
+    private List<Planet> planets;
 
-    Missile missile;
+    private Missile missile;
 
-    Planet chosenPlanet;
+    private Planet chosenPlanet;
     /**
      * Missile starting point. It starting point is the location of the player planet.
      */
@@ -50,12 +54,18 @@ public class BattlesSimulator {
      */
     private int enemyPlanetDeviation = 150;
 
-    double unstableRotationRatio = 0;
+    private double unstableRotationRatio = 0;
     /**
      * Displays the battles window.
      * This method render all the objects in it.
      */
-    public  void display(){
+    private Engine engine;
+
+    public BattlesSimulator(Engine engine){
+        this.engine = engine;
+    }
+
+    public void display(){
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(Constants.WINDOW_TITLE);
@@ -98,19 +108,19 @@ public class BattlesSimulator {
                     Constants.ENEMY_ZAKROS_PLANET_X,
                     Constants.ENEMY_ZAKROS_PLANET_Y)));
 
-        missile = new Missile(
+        this.missile = new Missile(
                 Constants.MISSILE_IMAGE,
                 Constants.MISSILE_WIDTH,
                 Constants.MISSILE_HEIGHT,
                 Constants.MISSILE_X,
                 Constants.MISSILE_Y);
 
-        missiles = new Group(missile.getImageView());
-        missiles.setEffect(new DropShadow(2, Color.color(1, 0, 0)));
+        this.missiles = new Group(this.missile.getImageView());
+        this.missiles.setEffect(new DropShadow(2, Color.color(1, 0, 0)));
 
         final Group foreground = new Group();
 
-        for (Planet planet : planets) {
+        for (Planet planet : this.planets) {
             foreground.getChildren().add(planet.getImageView());
         }
 
@@ -142,12 +152,12 @@ public class BattlesSimulator {
                 .build();
         itemsBox.setStyle("-fx-background-color: black;");
 
-        final Group root = new Group(background, foreground, missiles, itemsBox);
-        missiles.setVisible(false);
+        final Group root = new Group(background, foreground, this.missiles, itemsBox);
+        this.missiles.setVisible(false);
         Scene scene = new Scene(root, Constants.BATTLE_WINDOW_WIDTH, Constants.BATTLE_WINDOW_HEIGHT);
 
         window.setScene(scene);
-        scene.setOnMousePressed(e -> startAnimation(e.getX(), e.getY()));
+        scene.setOnMousePressed(e -> this.startAnimation(e.getX(), e.getY()));
         window.showAndWait();
     }
 
@@ -157,48 +167,48 @@ public class BattlesSimulator {
      * @param y coordinate of the enemy planet location.
      */
     private void startAnimation(double x, double y){
-        chosenPlanet = findPlanetByXY(x,y);
+        this.chosenPlanet = findPlanetByXY(x,y);
 
         if (!missileAnimationEnded){
-            missiles.setVisible(true);
+            this.missiles.setVisible(true);
         }
 
         if (chosenPlanet == null){
-            setMissileMovement(x, y);
-            missiles.setRotate(unstableRotationRatio+=2);
+            this.setMissileMovement(x, y);
+            this.missiles.setRotate(unstableRotationRatio+=2);
 
-            if (missilePointX == x-enemyPlanetDeviation && missilePointY == y-enemyPlanetDeviation){
-                missilePointX = Constants.PLAYER_PLANET_X-120;
-                missilePointY = Constants.PLAYER_PLANET_Y-160;
-                missiles.setVisible(false);
-                missileAnimation.stop();
-                missileAnimationEnded = true;
+            if (this.missilePointX == x-this.enemyPlanetDeviation && this.missilePointY == y-this.enemyPlanetDeviation){
+                this.missilePointX = Constants.PLAYER_PLANET_X-120;
+                this.missilePointY = Constants.PLAYER_PLANET_Y-160;
+                this.missiles.setVisible(false);
+                this.missileAnimation.stop();
+                this.missileAnimationEnded = true;
             }
         }
         else{
-            setMissileRotation(chosenPlanet.getY());
-            setMissileMovement(chosenPlanet.getX(), chosenPlanet.getY());
+            this.setMissileRotation(this.chosenPlanet.getY());
+            this.setMissileMovement(this.chosenPlanet.getX(), this.chosenPlanet.getY());
 
-            if (missilePointX == chosenPlanet.getX()-enemyPlanetDeviation && missilePointY ==chosenPlanet.getY()-enemyPlanetDeviation){
-                missilePointX = Constants.PLAYER_PLANET_X-120;
-                missilePointY = Constants.PLAYER_PLANET_Y-160;
-                missiles.setVisible(false);
-                missileAnimation.stop();
-                missileAnimationEnded = true;
+            if (this.missilePointX == this.chosenPlanet.getX()-this.enemyPlanetDeviation && this.missilePointY ==this.chosenPlanet.getY()-enemyPlanetDeviation){
+                this.missilePointX = Constants.PLAYER_PLANET_X-120;
+                this.missilePointY = Constants.PLAYER_PLANET_Y-160;
+                this.missiles.setVisible(false);
+                this.missileAnimation.stop();
+                this.missileAnimationEnded = true;
             }
         }
 
-        missileAnimation = TranslateTransitionBuilder.create()
-                .node(missiles)
-                .fromX(missilePointX)
-                .toX(missilePointX)
-                .fromY(missilePointY)
-                .toY(missilePointY)
+        this.missileAnimation = TranslateTransitionBuilder.create()
+                .node(this.missiles)
+                .fromX(this.missilePointX)
+                .toX(this.missilePointX)
+                .fromY(this.missilePointY)
+                .toY(this.missilePointY)
                 .duration(Duration.millis(0.1))
-                .onFinished(e -> startAnimation(x,y))
+                .onFinished(e -> this.startAnimation(x,y))
                 .build();
 
-        missileAnimation.play();
+        this.missileAnimation.play();
     }
 
     /**
@@ -207,18 +217,18 @@ public class BattlesSimulator {
      * @param y coordinate of the enemy planet location.
      */
     private void setMissileMovement(double x, double y) {
-        if(missilePointX < x - enemyPlanetDeviation){
-            missilePointX++;
+        if(this.missilePointX < x - this.enemyPlanetDeviation){
+            this.missilePointX++;
         }
-        else if (missilePointX > x - enemyPlanetDeviation){
-            missilePointX--;
+        else if (this.missilePointX > x - this.enemyPlanetDeviation){
+            this.missilePointX--;
         }
 
-        if (missilePointY < y - enemyPlanetDeviation){
-            missilePointY++;
+        if (this.missilePointY < y - this.enemyPlanetDeviation){
+            this.missilePointY++;
         }
-        else if (missilePointY > y - enemyPlanetDeviation){
-            missilePointY--;
+        else if (this.missilePointY > y - this.enemyPlanetDeviation){
+            this.missilePointY--;
         }
     }
 
@@ -228,15 +238,15 @@ public class BattlesSimulator {
      * @param planetY y coordinate of the enemy planet.
      */
     private void setMissileRotation(double planetY) {
-        if (missilePointY < planetY){
-            double ratio = planetY - missilePointY;
+        if (this.missilePointY < planetY){
+            double ratio = planetY - this.missilePointY;
             double degrees = (ratio / 10) - 15;
-            missiles.setRotate(degrees);
+            this.missiles.setRotate(degrees);
         }
-        else if (missilePointY > planetY){
-            double ratio = missilePointY - planetY;
+        else if (this.missilePointY > planetY){
+            double ratio = this.missilePointY - planetY;
             double degrees = (ratio / 10) + 260;
-            missiles.setRotate(degrees);
+            this.missiles.setRotate(degrees);
         }
     }
 
@@ -250,13 +260,13 @@ public class BattlesSimulator {
      */
     private Planet findPlanetByXY(double x, double y) {
 
-        Optional<Planet> matchedPlanet = planets
+        Optional<Planet> matchedPlanet = this.planets
                 .stream()
                 .filter(planet -> Math.pow(
-                        (x - (planet.getX() + deviationX)), 2)
-                        + Math.pow((y - (planet.getY() + deviationY)), 2)
+                        (x - (planet.getX() + this.deviationX)), 2)
+                        + Math.pow((y - (planet.getY() + this.deviationY)), 2)
                         <= Math.pow((planet.getWidth() / 2), 2)
-                        && planet != planets.get(0))
+                        && planet != this.planets.get(0))
                 .findFirst();
 
         try {
@@ -265,5 +275,33 @@ public class BattlesSimulator {
         catch (NoSuchElementException e){
             return null;
         }
+    }
+
+    private void battleProcess(){
+        long enemyMoolah = engine.getMoolah();
+
+        int enemyBaconExponent = EnhancedRandom.nextInt(
+                (engine.getBaconAmount().getExponent() / 2),
+                engine.getBaconAmount().getExponent() + (engine.getBaconAmount().getExponent() / 2));
+        double enemyBaconPrecision = EnhancedRandom.nextDouble(
+                (engine.getBaconAmount().getPrecision() / 2),
+                engine.getBaconAmount().getPrecision() + (engine.getBaconAmount().getPrecision() / 2));
+
+        int enemyFreedomExponent = EnhancedRandom.nextInt(
+                (engine.getFreedomAmount().getExponent() / 2),
+                engine.getFreedomAmount().getExponent() + (engine.getFreedomAmount().getExponent() / 2));
+        double enemyFreedomPrecision = EnhancedRandom.nextDouble(
+                (engine.getFreedomAmount().getPrecision() / 2),
+                engine.getFreedomAmount().getPrecision() + (engine.getFreedomAmount().getPrecision() / 2));
+
+        int enemyDemocracyExponent = EnhancedRandom.nextInt(
+                (engine.getDemocracyAmount().getExponent() / 2),
+                engine.getDemocracyAmount().getExponent() + (engine.getDemocracyAmount().getExponent() / 2));
+        double enemyDemocracyPrecision = EnhancedRandom.nextDouble(
+                (engine.getDemocracyAmount().getPrecision() / 2),
+                engine.getDemocracyAmount().getPrecision() + (engine.getDemocracyAmount().getPrecision() / 2));
+
+
+
     }
 }
---
+++ src/utils/EnhancedRandom.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.util.Random;
+
+public class EnhancedRandom {
+    private static Random random = new Random();
+
+    public static double nextDouble(double start, double end){
+        double randomValue = start + (end - start) * random.nextDouble();
+        return randomValue;
+    }
+
+    public static int nextInt(int start, int end){
+        int randomValue = start + (end - start) * random.nextInt();
+        return randomValue;
+    }
+}
