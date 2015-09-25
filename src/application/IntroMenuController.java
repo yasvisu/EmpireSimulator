7770b9add89daab7de61413710543b8bf3cac762
yasvisu <yasvisu@gmail.com>
2015-09-25 10:52:26 +0300

    #7 Tidied IntroMenuController imports.

--- src/application/IntroMenuController.java
+++ src/application/IntroMenuController.java
@@ -6,7 +6,6 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
